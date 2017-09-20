8a4200b2219af6a7fbf4d4ec66f0ceabb75b5437
AJ Leva <31457535+AnthonyLeva@users.noreply.github.com>
2017-09-20 15:51:04 -0400

    Add files via upload

--- part_iii.py
+++ part_iii.py
@@ -1,4 +1,13 @@
 # pylint: disable=w,c
 # Problem Set 1, Part III
-# Name:Leva, Anthony
-# Collaborators:
+# Name: Leva, Anthony
+# Collaborators: Office hours
+
+
+def is_even(input_int):
+    answer = input_int % 2
+    return answer == 0
+
+
+is_even(3)
+is_even(20)
